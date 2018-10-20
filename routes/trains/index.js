4aa329ab2762636e45810b843ab1e24cccbdd13a
Roman Kuzmenko <kuzmrom7@gmail.com>
2018-10-20 16:41:05 +0300

    add dynamic uri and mock data

--- routes/trains/index.js
+++ routes/trains/index.js
@@ -1,4 +1,5 @@
 const { setupURL, getRid, timeout } = require("./utils");
+const { data } = require("../../utils/responseExample");
 
 const express = require("express");
 const router = express.Router();
@@ -6,19 +7,32 @@ const request = require("request");
 const cookieParser = require("cookie-parser");
 
 router.get("/", (req, res) => {
-  request(getRid(), async (error, response, bodyRid) => {
-    try {
-      const bodyParser = await JSON.parse(bodyRid);
-      const cookie = await cookieParser.JSONCookies(response.headers);
-      const rid = bodyParser.rid;
-      await timeout(1000);
-      request(setupURL(rid, cookie), async (error, resp, body) => {
-        res.json(JSON.parse(body));
-      });
-    } catch (e) {
-      console.log("error:", e);
+  const query = req.query;
+  request(
+    getRid(query.from, query.to, query.date),
+    async (error, response, bodyRid) => {
+      try {
+        const bodyParser = await JSON.parse(bodyRid);
+        const cookie = await cookieParser.JSONCookies(response.headers);
+        const rid = bodyParser.rid;
+        await timeout(1000);
+        request(setupURL(rid, cookie), async (error, resp, body) => {
+          res.setHeader("Content-Type", "application/json");
+          res.setHeader("Access-Control-Allow-Origin", "*");
+          res.send(JSON.stringify(JSON.parse(body)));
+        });
+      } catch (e) {
+        console.log("error:", e);
+        res.json({
+          error: e
+        });
+      }
     }
-  });
+  );
+});
+
+router.get("/dev", (req, res) => {
+  res.json(data);
 });
 
 module.exports = router;
--- routes/trains/utils.js
+++ routes/trains/utils.js
@@ -1,5 +1,7 @@
-const getRid = () => {
-  return "https://pass.rzd.ru/timetable/public/ru?STRUCTURE_ID=735&layer_id=5371&dir=0&tfl=3&refererPageId=4819&checkSeats=1&code0=2004001&dt0=20.10.2018&code1=2000000";
+const getRid = (from, to, date) => {
+  return `https://pass.rzd.ru/timetable/public/ru?STRUCTURE_ID=735&layer_id=5371&dir=0&tfl=3&refererPageId=4819&checkSeats=1&code0=${encodeURI(
+    from
+  )}&dt0=${encodeURI(date)}&code1=${encodeURI(to)}`;
 };
 const getTrains = rid => {
   return `http://pass.rzd.ru/timetable/public/ru?STRUCTURE_ID=735&layer_id=5371&rid=${rid}`;
